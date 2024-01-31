276583df44130baed48c055be5eaa8e3d7e8be60
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-02-01 04:17:00 +0800

    feat: update status

--- req/notify.go
+++ req/notify.go
@@ -1,7 +1,9 @@
 package req
 
+import "github.com/open4go/req5rsp/cst"
+
 // OrderNotify 订单通知
 type OrderNotify struct {
-	OrderId string `json:"order_id" binding:"required"` // 订单主键id
-	Status  int    `json:"status"`
+	OrderId string          `json:"order_id" binding:"required"` // 订单主键id
+	Status  cst.OrderStatus `json:"status"`
 }
