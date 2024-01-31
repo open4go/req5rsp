febe6a43b2a0f93e87848c59014ad1cea09e427a
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-02-01 03:22:26 +0800

    feat: add order status

--- cst/order.go
+++ cst/order.go
@@ -1,1 +1,35 @@
 package cst
+
+// OrderStatus 订单状态
+type OrderStatus int
+
+const (
+	// OrderInit 订单初始化(未完成支付）
+	OrderInit OrderStatus = iota
+	// OrderPaid 订单已付款
+	OrderPaid
+	// OrderMaking 订单制作中
+	OrderMaking
+	// OrderProduceCompleted 订单制作完成
+	OrderProduceCompleted
+	// OrderCompleted 订单完成 （已经自提/堂食配送完成）
+	OrderCompleted
+	// OrderCancel 订单取消
+	OrderCancel
+	// OrderCancelApproved 订单取消 （商家同意）
+	OrderCancelApproved
+	// OrderCancelCompleted 订单取消（完成）已经退款
+	OrderCancelCompleted
+	// OrderTakeoutPending 外卖 （等待接单 ）
+	OrderTakeoutPending
+	// OrderTakeoutConfirmed 外卖（已经接单）
+	OrderTakeoutConfirmed
+	// OrderTakeoutUnConfirmed 外卖（放弃接单）
+	OrderTakeoutUnConfirmed
+	// OrderTakeoutTake 外卖（已经取货）
+	OrderTakeoutTake
+	// OrderTakeoutDone 外卖（已经送达）
+	OrderTakeoutDone
+	// OrderTakeoutComment 订单已经评论
+	OrderTakeoutComment
+)
