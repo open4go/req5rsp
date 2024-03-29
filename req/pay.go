72c7456752181cc34dcc610f7e29f7613793ce75
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-03-30 05:04:28 +0800

    add more comment

--- req/pay.go
+++ req/pay.go
@@ -30,16 +30,19 @@ type CloseRequest struct {
 
 // ScanPayRequest 扫码支付
 type ScanPayRequest struct {
-	// 支付渠道
+	// 支付渠道 (由前台根据用户支付偏好，在点餐机上完成勾选）
 	Channel cst.ChannelType `form:"channel" json:"channel" xml:"channel" binding:"required"`
-	// 订单号
+	// 订单号 (发起下单后会自动生成订单号并且在支付时自动填入该字段)
 	OrderID string `form:"order_id" json:"order_id" xml:"order_id" binding:"required"`
-	// 订单描述
+	// 订单描述 （订单自动生成）
 	Desc string `form:"desc" json:"desc" xml:"desc" binding:"required"`
-	// 金额 (单位:分）
+	// 金额 (单位:分）（订单自动生成）
 	Amount int64 `form:"amount" json:"amount" xml:"amount"  binding:"required"`
-	// 订单发生地点 (门店id；渠道：微信；...)
+	// 订单发生地点 (门店id；渠道：微信；...) （订单自动生成）
 	At string `form:"at" json:"at" xml:"at"`
-	// 订单发生地点 (门店id；渠道：微信；...)
+	// 由扫码枪通过扫客户提供的支付二维码生成
 	Code string `form:"code" json:"code" xml:"code"`
+	// 用户可能会需要积分，因此这里可以是手机号或者账号（一般用户只会记住手机号）
+	// 这里不限定死，后台或者增加一个账号类型以便区分
+	UserID string `form:"user_id" json:"user_id" xml:"user_id"`
 }
