2855e5999e41867d1ae688a2a20ffd844de62a9e
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-04-10 05:06:52 +0800

    add pay method

--- cst/pay.go
+++ cst/pay.go
@@ -17,3 +17,20 @@ const (
 	// UniPay 银联支付
 	UniPay
 )
+
+type PayMethod int
+
+const (
+	// WxMiniPay 微信小程序支付
+	WxMiniPay PayMethod = iota
+	// WxScanQRCode 微信扫码支付
+	WxScanQRCode
+	// AliMiniPay 支付宝小程序支付
+	AliMiniPay
+	// AliScanQRCode 支付宝扫码支付
+	AliScanQRCode
+)
+
+func (p PayMethod) String() string {
+	return [...]string{"微信小程序支付", "微信扫码支付", "支付宝小程序支付", "支付宝扫码支付"}[p]
+}
