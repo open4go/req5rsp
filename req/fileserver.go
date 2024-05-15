8ed3e8cb433aa1bfe06e1c3b51c9f0051ec78bac
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-05-16 01:49:44 +0800

    update payload for file bind

--- req/fileserver.go
+++ req/fileserver.go
@@ -12,3 +12,37 @@ type BindFile struct {
 	// 最大尺寸
 	MaxSize string `form:"max_size" json:"max_size" xml:"max_size"`
 }
+
+// UploadFileInfo 文件rpc 更新绑定请求
+type UploadFileInfo struct {
+	// 目标id(待更新到目标数据的id）
+	// 例如：商品的id
+	Id string `form:"id" json:"id" xml:"id"`
+	// 更新到该目标数据的具体字段
+	// 例如：商品中的image字段
+	Field string `form:"field" json:"field" xml:"field"`
+	// 图片的具体信息
+	// 包含url 等
+	Payload Response `form:"payload" json:"payload" xml:"payload"`
+}
+
+type Response struct {
+	// 文件md5 id
+	Id string `form:"id" json:"id" xml:"id"`
+	// 文件md5 id
+	ContentID string `form:"content_id" json:"content_id" xml:"content_id"`
+	// 文件名
+	Name string `form:"name" json:"name" xml:"name"`
+	// 文件路径
+	Format string `form:"format" json:"format" xml:"format"`
+	// 文件路径
+	Path string `form:"path" json:"path" xml:"path"`
+	// 文件大小
+	Size uint `form:"size" json:"size" xml:"size"`
+	// 文件地址
+	Url string `form:"url" json:"url" xml:"url"`
+	// 文件大小
+	Height int `form:"height" json:"height" xml:"height"`
+	// 文件大小
+	Width int `form:"width" json:"width" xml:"width"`
+}
