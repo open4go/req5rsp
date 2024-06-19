40c2ee4c72a796b1de9520e8beda33db483d49d7
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-20 02:52:45 +0800

    add propes

--- req/property.go
+++ req/property.go
@@ -2,6 +2,13 @@ package req
 
 // CheckPropertyPrice 查询规格价格
 type CheckPropertyPrice struct {
+	// PropsSelected 勾选的项
+	PropsSelected []CodeWithIDS `form:"props_selected" json:"props_selected" xml:"props_selected"`
+}
+
+type CodeWithIDS struct {
 	// Code 需要查询的id
-	Code []string `form:"code" json:"code" xml:"code"  binding:"required"`
+	Code string `form:"code" json:"code" xml:"code"  binding:"required"`
+	// 选项列表
+	IDList []int `form:"id_list" json:"id_list" xml:"id_list"  binding:"required"`
 }
