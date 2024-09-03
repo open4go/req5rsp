9ad9fb559afc341ae709d389a814e3f65d63f4c4
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-09-04 01:55:31 +0800

    add inivite

--- rsp/user.go
+++ rsp/user.go
@@ -4,3 +4,8 @@ package rsp
 type UserPhoneResponse struct {
 	Phone string `form:"phone" json:"phone" xml:"phone"  binding:"required"`
 }
+
+// InviteResponse 用户手机号转换
+type InviteResponse struct {
+	Code string `form:"code" json:"code" xml:"code"  binding:"required"`
+}
