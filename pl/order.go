b8bc9f5956628f83a0b285a3305cbb3c95df7649
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2025-11-18 00:19:04 +0800

    add more field

--- pl/order.go
+++ pl/order.go
@@ -2,6 +2,18 @@ package pl
 
 // payload for order
 
+// EventFrom 事件来源
+type EventFrom int
+
+const (
+	// Callback 回调事件
+	Callback EventFrom = iota
+	// PayResponse 支付成功返回
+	PayResponse
+	// QueryRequest 主动查询订单触发
+	QueryRequest
+)
+
 // OrderPaySuccess 支付成功关键信息
 type OrderPaySuccess struct {
 	// 渠道
@@ -18,8 +30,14 @@ type OrderPaySuccess struct {
 	OrderNo string `json:"order_no"`
 	// 支付金额字符串
 	Amount string `json:"amount"`
+	// 币种
+	Currency string `json:"currency"`
+	// 单位
+	Unit string `json:"unit"`
 	// 支付金额数字
 	AmountNum int64 `json:"amount_num"`
 	// 时间
 	Time int64 `json:"time"`
+	// 事件来源
+	Event EventFrom `json:"event"`
 }
